a9d847686feafa8e9b62b96c710ed86ac24a17a6
Tom Moya Schiller <moya@asofterspace.com>
2020-06-06 13:50:12 +0200

    More RAM, more RAM, more RAM for the pirates!

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 
 cd /D %~dp0
 
-java -classpath "%~dp0\bin" -Xms16m -Xmx1024m com.asofterspace.assMusician.AssMusician %*
+java -classpath "%~dp0\bin" -Xms16m -Xmx2048m com.asofterspace.assMusician.AssMusician %*
 
 pause
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd `dirname "$0"`
 
-java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx1024m com.asofterspace.assMusician.AssMusician "$@"
+java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx2048m com.asofterspace.assMusician.AssMusician "$@"
