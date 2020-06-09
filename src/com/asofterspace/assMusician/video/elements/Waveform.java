a440294bd108f81be4ded6e2f0da720d32a2b589
Tom Moya Schiller <moya@asofterspace.com>
2020-06-09 23:08:47 +0200

    Fade in waveform on the left

--- src/com/asofterspace/assMusician/video/elements/Waveform.java
+++ src/com/asofterspace/assMusician/video/elements/Waveform.java
@@ -23,7 +23,9 @@ public class Waveform {
 		int[] leftData = soundData.getLeftData();
 		int[] rightData = soundData.getRightData();
 
-		for (int x = 0; x < width; x++) {
+		int leftOffset = 28;
+
+		for (int x = leftOffset; x < width; x++) {
 			int loudMax = 0;
 			int loudMin = 0;
 			for (long i = ((step + x - (width/2)) * (long) leftData.length) / totalFrameAmount; i < ((1 + step + x - (width/2)) * (long) leftData.length) / totalFrameAmount; i++) {
@@ -48,11 +50,22 @@ public class Waveform {
 			int top = vertPos - ((loudMax * 64) / (8*16*16*16));
 			int mid = vertPos;
 			int bottom = vertPos - ((loudMin * 64) / (8*16*16*16));
-			if (x == width / 2) {
-				img.drawLine(x, top, x, bottom, highlightColor);
+			if (x < leftOffset + 10) {
+				// for the first ten pixels, fade in the waveform
+				ColorRGB black = new ColorRGB(0, 0, 0);
+				if (x == width / 2) {
+					img.drawLine(x, top, x, bottom, ColorRGB.intermix(highlightColor, black, (x - leftOffset) / 10.0));
+				} else {
+					img.drawLine(x, mid, x, bottom, ColorRGB.intermix(midgroundColor, black, (x - leftOffset) / 10.0));
+					img.drawLine(x, top, x, mid, ColorRGB.intermix(foregroundColor, black, (x - leftOffset) / 10.0));
+				}
 			} else {
-				img.drawLine(x, mid, x, bottom, midgroundColor);
-				img.drawLine(x, top, x, mid, foregroundColor);
+				if (x == width / 2) {
+					img.drawLine(x, top, x, bottom, highlightColor);
+				} else {
+					img.drawLine(x, mid, x, bottom, midgroundColor);
+					img.drawLine(x, top, x, mid, foregroundColor);
+				}
 			}
 		}
 	}
